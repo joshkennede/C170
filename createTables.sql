08951582a3377c08b37cd2588db968d9ce1e4953
Joshua Kennedy <joshkennede@gmail.com>
2016-10-14 08:18:38 -0700

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,24 +1,24 @@
 CREATE TABLE customer(
- customerID INT NOT NULL AUTO_INCREMENT,
- lastName VARCHAR(150),
- firstName VARCHAR(150),
+ customerID    INT NOT NULL AUTO_INCREMENT,
+ lastName      VARCHAR(150),
+ firstName     VARCHAR(150),
  streetAddress VARCHAR(255),
- aptNumber VARCHAR(25),
- city VARCHAR(100),
- state VARCHAR(50),
- zipCode INT,
- homePhone VARCHAR(50),
- mobilePhone VARCHAR(50),
- otherPhone VARCHAR(50),
+ aptNumber     VARCHAR(25),
+ city          VARCHAR(100),
+ state         VARCHAR(50),
+ zipCode       INT,
+ homePhone     VARCHAR(50),
+ mobilePhone   VARCHAR(50),
+ otherPhone    VARCHAR(50),
 
  PRIMARY KEY (customerID)
 );
 
 CREATE TABLE salesOrder(
- orderID INT NOT NULL AUTO_INCREMENT,
- customerID INT NOT NULL,
- notes VARCHAR(255),
- orderDate DATE,
+ orderID        INT NOT NULL AUTO_INCREMENT,
+ customerID     INT NOT NULL,
+ notes          VARCHAR(255),
+ orderDate      DATE,
 
  PRIMARY KEY (orderID),
 
@@ -27,18 +27,18 @@ CREATE TABLE salesOrder(
 );
 
 CREATE TABLE donut(
- donutID INT NOT NULL AUTO_INCREMENT,
- donutName VARCHAR(100),
+ donutID          INT NOT NULL AUTO_INCREMENT,
+ donutName        VARCHAR(100),
  donutDescription VARCHAR(150),
- donutPrice DECIMAL(4,2),
+ donutPrice       DECIMAL(4,2),
   
  PRIMARY KEY (donutID)
 );
 
 CREATE TABLE quantity(
- donutID INT NOT NULL,
- orderID INT NOT NULL,
- quantity INT,
+ donutID     INT NOT NULL,
+ orderID     INT NOT NULL,
+ quantity    INT,
 
  PRIMARY KEY (donutID, orderID),
 
